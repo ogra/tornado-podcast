f164cbbd70687950a5c762ba861542c9c23fe656
Toshiyuki Ogura <tos.ogura@gmail.com>
2015-03-29 18:48:07 +0900

    add test_homehandler_admin

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 import app
- 
+
+import mock
 from tornado.escape import to_unicode
 from tornado.testing import AsyncHTTPTestCase, gen_test
 from tornado.httpclient import HTTPError
- 
- 
+
+
 class AppTest(AsyncHTTPTestCase):
     def setUp(self):
         super(AppTest, self).setUp()
         print("setup")
- 
+
     def get_app(self):
         return app.application
- 
+
     @gen_test(timeout=10)
     def test_homehandler(self):
         response = yield self.http_client.fetch(self.get_url('/'))
         self.assertIn("Podcast Home", to_unicode(response.body))
- 
+
+    @gen_test(timeout=10)
+    def test_homehandler_admin(self):
+        self.get_app().sessions = mock.MagicMock()
+        self.get_app().sessions.get_session.return_value = 'test_cookie'
+        response = yield self.http_client.fetch(self.get_url('/'))
+        self.assertIn("Log Out", to_unicode(response.body))
+
     @gen_test
     def test_404(self):
         with self.assertRaises(HTTPError) as cm:
             yield self.http_client.fetch(self.get_url('/nonexistent'))
         self.assertEqual(cm.exception.code, 404)
- 
+
     def tearDown(self):
         print("teardown")
         super(AppTest, self).tearDown()
